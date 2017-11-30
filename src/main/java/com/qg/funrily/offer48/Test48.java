857bcd011c2325eba298ce6a7ea243ab6ba1aff5
FunriLy <fr479910770@qq.com>
2017-11-30 11:09:01 +0800

    不能被继承的类

---
+++ src/main/java/com/qg/funrily/offer48/Test48.java
@@ -0,0 +1,15 @@
+package com.qg.funrily.offer48;
+
+/**
+ * 面试题48：不能被继承的类
+ * @author FunriLy
+ * Created by FunriLy on 2017/11/30.
+ * From small beginnings comes great things.
+ */
+public class Test48 {
+
+    /*
+    Java 中使用关键字 final 实现不能被继承的类
+    C# 中使用关键字 sealed 实现不能被继承的类
+     */
+}
